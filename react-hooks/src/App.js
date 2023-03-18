9134f6b6e0bf5e0229535987e1d849313086195e
Parkash1308 <parkash.official1308@gmail.com>
2023-03-18 20:59:22 +0500

    init

---
+++ react-hooks/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Hook from "./componant/hooks";
+
+function App() {
+  return (
+    <div className="App">
+    <Hook/>
+    </div>
+  );
+}
+
+export default App;
